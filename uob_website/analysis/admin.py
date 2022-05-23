6a48237b77a01b28edd299eef5b716b0c2565b7a
tong <425663303@qq.com>
2022-05-23 18:05:16 +0800

    ver0.6.2: add sql for init db

--- uob_website/analysis/admin.py
+++ uob_website/analysis/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Audio, AnalysisSelection, User, STTresult
+from .models import Audio, AnalysisSelection, STTresult
 
 admin.site.register(Audio)
 admin.site.register(STTresult)
 
 admin.site.register(AnalysisSelection)
 
-admin.site.register(User)
